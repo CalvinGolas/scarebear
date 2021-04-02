7b5e1b9bf839dcfcc3f7fc67117819c900622b0d
CalvinGolas <calvingolas@gmail.com>
2021-04-02 02:59:59 -0600

    finalized bash script

--- external-tools/UpdateStories.sh
+++ external-tools/UpdateStories.sh
@@ -1,15 +1,17 @@
 #find a random story to run
-#TODO: cd into story directory and get complete path of one story dir
+#cd into story directory and get complete path of one story dir
+cd /home/pi/stories/scare-bear-stories
 storyDir= ls -d $PWD/* | shuf -n 1
 #run the story
 cd $storyDir
-cp $ #TODO: value of bearcontrol.py bearcontrol.py
-python main.py 
+cp $ ../../capstone/bearcontrol.py bearcontrol.py
+python Main.py 
 
 #update actions
 rm bearcontrol.py
 git pull
-#TODO: cd into main git folder and pull to update
+#cd into main git folder and pull to update
+cd ../../capstone
 git pull
 #now that we're all done, we can power off the pi safely
 shutdown
